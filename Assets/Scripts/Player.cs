424c833a6f630048ceac16d9e136754c690f7658
Yeltsin <wixovmvoxiw@mail.ru>
2026-01-02 15:50:17 +0300

    Old movement system
    Применение старой системы вводы для передвижения игрока

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 7f;
+    void Update()
+    {
+        Vector3 inputVector = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.z = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.z = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if ( Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+        transform.position += inputVector * Time.deltaTime * moveSpeed;
+    }
+}
